6606cccaca537b165190fa514cca7be74b8ea004
maxmmyron <maxmmyron@gmail.com>
2019-10-08 23:17:28 -0400

    added new surface and style settings in game constructor

--- src/game.js
+++ src/game.js
@@ -6,10 +6,19 @@ export default class Game {
     constructor(WORLD_CONSTRAINTS){
         this.gameWidth = WORLD_CONSTRAINTS.DIM.WIDTH;
         this.gameHeight = WORLD_CONSTRAINTS.DIM.HEIGHT;
+
+        this.surface_settings = {
+            color: WORLD_CONSTRAINTS.SURFACE_SETTINGS.COLOR,
+            height: WORLD_CONSTRAINTS.SURFACE_SETTINGS.HEIGHT
+        };
+
+        this.default_styles = {
+            fillStyle: WORLD_CONSTRAINTS.DEFAULT_STYLES.FILL_STYLE
+        };
     }
 
     start(){
-        this.surface = new RigidSurface(this, 50, "#000000");
+        this.surface = new RigidSurface(this);
         this.player = new Player(this);
 
         this.gameObjects = [
@@ -25,10 +34,9 @@ export default class Game {
     }
 
     draw(fps, ctx){
-        ctx.fillStyle= '#000000';
+        ctx.fillStyle= this.default_styles.fillStyle;
         if(isNaN(fps) == false) ctx.fillText("FPS: " + Math.ceil(fps), 5, 15);
     
         this.gameObjects.forEach(object => object.draw(ctx));
-    
     }
 }
