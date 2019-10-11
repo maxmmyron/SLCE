5cfaeccd4a41169ee6a9309e5450da952ac51fad
maxmmyron <maxmmyron@gmail.com>
2019-10-11 00:20:58 -0400

    changed name of constructor argument

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,12 +1,12 @@
 export default class InputHandler {
-    constructor(player){
+    constructor(object){
 
         document.body.addEventListener("keydown", function (e) {
-            player.keyBuffer[e.keyCode] = true;
+            object.keyBuffer[e.keyCode] = true;
         });
 
         document.body.addEventListener("keyup", function (e) {
-            player.keyBuffer[e.keyCode] = false;
+            object.keyBuffer[e.keyCode] = false;
         });
     }
 }
