392044d6c605c3485e6d3a363dafeee7231c26fe
maxmmyron <maxmmyron@gmail.com>
2019-10-11 20:00:03 -0400

    refactor; added circle collision and fixed box collision

--- src/physics/objectCollision.js
+++ src/physics/objectCollision.js
@@ -1,36 +1,20 @@
 export default class objectCollision{
-    constructor(object1, object2){
-        this.object1 = object1;
-        this.object2 = object2;
+    constructor(){}
+
+    checkBoxCollision(box1, box2){
+        if( box1.position.x < box2.position.x + box2.size.width &&
+            box1.position.x + box1.size.width > box2.position.x &&
+            box1.position.y < box2.position.y + box2.size.height &&
+            box1.position.y + box1.size.height > box2.position.y ) box1.style.color = "#00FF00"; 
+        else box1.style.color = "#FF00FF";
     }
 
-    checkCollision(){
-        let object1BoundingBox = this.getCornersOfObject(this.object1);
-        let object2BoundingBox = this.getCornersOfObject(this.object2);
-    }
+    checkCircleCollision(circle1, circle2){
+        let dx = circle1.position.x - circle2.position.x;
+        let dy = circle1.position.y - circle2.position.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
 
-    getCornersOfObject(object){
-        let topLeft = {
-            x: object.position.x, 
-            y: object.position.y
-        };
-        let topRight = {
-            x: object.position.x + object.size.width, 
-            y: object.position.y
-        };
-        let bottomLeft = {
-            x: object.position.x, 
-            y: object.position.y + object.size.height
-        };
-        let bottomRight = {
-            x: object.position.x + object.size.width, 
-            y: object.position.y + object.size.height
-        };
-        return {
-            topLeft: topLeft,
-            topRight: topRight,
-            bottomLeft: bottomLeft,
-            bottomRight: bottomRight
-        };
+        if(distance < circle1.size.radius + circle2.size.radius) circle1.style.color = "#FF0F0F";
+        else circle1.style.color = "#00FFAC";
     }
 }
