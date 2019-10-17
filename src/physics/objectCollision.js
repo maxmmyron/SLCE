d2ff9ee304474003e18e248312cbc36bbcb916af
maxmmyron <maxmmyron@gmail.com>
2019-10-16 23:10:30 -0400

    small refactor
    updated argument values

--- src/physics/objectCollision.js
+++ src/physics/objectCollision.js
@@ -4,11 +4,11 @@ export default class objectCollision{
     }
 
     circleCollision(circle1, circle2){
-        let dx = circle1.pos.x - circle2.pos.x;
-        let dy = circle1.pos.y - circle2.pos.y;
+        let dx = circle1.x - circle2.x;
+        let dy = circle1.y - circle2.y;
         let distance = Math.sqrt(dx * dx + dy * dy);
 
-        if(distance < circle1.size.radius + circle2.size.radius) circle1.style.color = "#FF0F0F";
+        if(distance < circle1.radius + circle2.radius) circle1.style.color = "#FF0F0F";
         else circle1.style.color = "#00FFAC";
     }
 }
