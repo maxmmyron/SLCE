dedc9bc28c52914809a268d6100f6b51b6eb9ec4
maxmmyron <maxmmyron@gmail.com>
2019-10-16 23:12:36 -0400

    updated arguments, removed old code and replaced with new code.

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 import RigidSurface from "./objects/RigidSurface.js";
-import RigidObject from "./objects/RigidObject.js";
-import InputHandler from "./handlers/inputHandler.js/index.js";
-import ObjectCollision from "./physics/objectCollision.js";
+import InputHandler from "./handlers/inputHandler.js";
 import CircleObject from "./objects/CircleObject.js";
+import Shape from "./objects/Shape.js";
+import RigidShape from "./objects/RigidShape.js";
 
 export default class Game {
     constructor(WORLD_CONSTRAINTS){
@@ -21,16 +21,22 @@ export default class Game {
 
     start(ctx){
         this.surface = new RigidSurface(this);
-        this.rigidObject = new RigidObject(this, 505, 55, 300, 100, "#A0C3F0");
-        this.player = new RigidObject(this, 25, 50, 500, 600, "#FF00FF");
 
         this.circle1 = new CircleObject(this, 15, "#00FFAC", 700, 400);
         this.circle2 = new CircleObject(this, 25, "#A0C34F", 500, 600);
+
+        this.rigidObject = new RigidShape([[0,0], [0,55], [505,55], [505,0]], 300, 100, "#A0C3F0");
+        this.player = new Shape(this, [[0,0],[0,50],[25,50],[25,0]], 500, 600, "#FF00FF");
+
+        this.triangle1 = new Shape(this, [[30, 0], [60, 60], [0, 60]], 50, 50, "#F5A88C");
+        this.pentagon1 = new Shape(this, [[25,0], [0,25], [10,50], [40,50], [50,25]], 700, 700, "#71A84F");
 
         this.gameObjects = [ //renders in order from [0] => [gameObjects.length]. Place always on top objects at end of array.
             this.rigidObject,
             this.circle1,
             this.circle2,
+            this.triangle1,
+            this.pentagon1,
             this.player,
             this.surface
         ];
@@ -39,14 +45,14 @@ export default class Game {
         new InputHandler(this.player);
 
         //creates a new collision system.
-        this.rigidCollider = new ObjectCollision(ctx);
+        //this.rigidCollider = new ObjectCollision(ctx);
     }
 
     update (deltaTime){
         this.gameObjects.forEach(object => object.update(deltaTime));
         //this.rigidCollider.rectCollision(this.player, this.rigidObject);
-        this.rigidCollider.circleCollision(this.circle1, this.circle2);
-        this.rigidObject.collideRectangle(this.player);
+        //this.rigidCollider.circleCollision(this.circle1, this.circle2);
+        //this.rigidObject.collideRectangle(this.player);
     }
 
     draw(fps, ctx){
