e843b28792b681aa2b483532de6908db5dec8c39
maxmmyron <maxmmyron@gmail.com>
2019-10-20 23:00:54 -0400

    readded canvas bounds detection changed code to accept any convex polygon

--- src/handlers/Controller.js
+++ src/handlers/Controller.js
@@ -14,34 +14,49 @@ export default class Controller {
             x: 0,
             y: 0
         };
+
+        this.touching = false;
     }
 
     checkMove(buffer){
-        if(buffer[87] && this.vel.y >= -this.constraints.maxSpeed) this.vel.y--;
+        if(buffer[87] && this.vel.y >= -this.constraints.maxSpeed && this.touching == true) {
+            this.vel.y = -30;
+            this.touching = false;
+        }
         if(buffer[83] && this.vel.y < this.constraints.maxSpeed) this.vel.y++;
-        if(buffer[65] && this.vel.x >= -this.constraints.maxSpeed) this.vel.x--;
-        if(buffer[68] && this.vel.x <= this.constraints.maxSpeed) this.vel.x++;
+        if(buffer[65] && this.vel.x >= -this.constraints.maxSpeed) this.vel.x =- 10;
+        if(buffer[68] && this.vel.x <= this.constraints.maxSpeed) this.vel.x =+ 10;
     }
 
-    /*checkWallHit(){
-        if(this.y < 0){
-            this.vel.y = 0;
-            this.y = 0;
+    checkWallHit(obj){
+        let xPoints = [], yPoints = [];
+        for(var i = 0; i < obj.points.length; i++){
+            xPoints.push(obj.points[i].x);
+            yPoints.push(obj.points[i].y);
         }
 
-        if(this.y > this.gameHeight - this.size.height - this.game.surface_settings.height){
-            this.vel.y = 0;
-            this.y = this.gameHeight - this.size.height - this.game.surface_settings.height;
-        }
+        xPoints.sort(); 
+        let sX = xPoints[0], mX = xPoints[xPoints.length - 1];
+        yPoints.sort();
+        let sY = yPoints[0], mY = yPoints[yPoints.length - 1];
+        
 
-        if(this.x < 0){
-            this.vel.x = 0;
-            this.x = 0;
+        if(obj.x < 0){
+            obj.vel.x = 0;
+            obj.x = 0;
         }
-
-        if(this.x > this.gameWidth - this.size.width){
-            this.vel.x = 0;
-            this.x = this.gameWidth - this.size.width;
+        if(obj.y < 0){
+            obj.vel.y = 0;
+            obj.y = 0;
         }
-    }*/
+        if(obj.x + mX > this.game.gameWidth){
+            obj.vel.x = 0;
+            obj.x = this.game.gameWidth - mX;
+        }
+        if(obj.y + mY > this.game.gameHeight){
+            obj.vel.y = 0;
+            obj.y = this.game.gameHeight - mY;
+            this.touching = true;
+        }
+    }
 }
