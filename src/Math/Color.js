dd46ec3fafd8befdba7774b7f6c53da2f6d37228
maxmmyron <maxmmyron@gmail.com>
2019-11-02 18:43:04 -0400

    seperated this from game.js to allow for cross file use.

---
+++ src/Math/Color.js
@@ -0,0 +1,11 @@
+export default class Color{
+
+    getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+}
