d63d0396104cf9983334f529738d84a50483fead
maxmmyron <maxmmyron@gmail.com>
2019-11-03 12:14:54 -0500

    minor changes

--- main.js
+++ main.js
@@ -12,18 +12,25 @@ let ctx = canvas.getContext('2d'); //define a new drawing context on our canvas
 
 let game = new Game(canvas); //create a new game
 
-game.add(new circleObject(game, 5, new Color().getRandomColor(), 500, 500, 200, 0, 0));
-game.add(new circleObject(game, 5, new Color().getRandomColor(), 1000, 250, 15, 0, 0));
+//game.add(new circleObject(game, 5, new Color().getRandomColor(), 500, 500, 200, 0, 0));
+//game.add(new circleObject(game, 5, new Color().getRandomColor(), 1500, 600, 10000, 0, 0));
+//game.add(new circleObject(game, 5, new Color().getRandomColor(), 1000, 250, 15, 0, 0));
 
-game.nBodySimulator = new nBody(1, 0.001, 100, game.gameObjects);
+for(var i = 0; i < 100; i++){
+    game.add(new circleObject(
+        game, Math.floor(Math.random() * 10) + 5,
+        new Color().getRandomColor(),
+        Math.random() * game.gameWidth,
+        Math.random() * game.gameHeight,
+        Math.floor(Math.random() * 10)
+    ));
+}
+
+game.nBodySimulator = new nBody(1, 0.001, 0.15, game.gameObjects);
 
 let simulatorUpdates = [
-    game.nBodySimulator.updatePositionVectors(),
-    game.nBodySimulator.updateVelocityVectors(),
-    game.nBodySimulator.updateAccelerationVectors()
+    game.nBodySimulator
 ];
-
-game.start(ctx); //start the game.
 
 start(game, ctx, simulatorUpdates); //start the gameLoop
 
