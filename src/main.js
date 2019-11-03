40daad92b171618b11b76c577359e06504e5580f
maxmmyron <maxmmyron@gmail.com>
2019-11-03 00:18:29 -0400

    large refactor

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
-/**
- * The main source code.
- * This implements the basic structure of the page, and mostly serves to size the canvas to a proper size, 
- * implement some obligatory DPI fixes, and initalizing the game to run. The world settings are also defined
- * here, which should probably be moved one day into it's own World class. :/
- */
-
+//imports
 import Game from "./core/game.js";
 import {start} from "./core/gameLoop.js";
+import circleObject from "./Objects/Circle.js";
+import Color from "./Math/Color.js";
+import nBody from "./Physics/nBody.js";
 
-//define a new canvas variable from which the game is crafted upon
+//define a new canvas given a canvas in DOM
 let canvas = document.getElementById('gameCanvas');
 
-let ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d'); //define a new drawing context on our canvas
 
-//create a new game given the world constrains and variables.
+let game = new Game(canvas); //create a new game
 
-let game = new Game(document.getElementById('gameCanvas'));
+game.add(new circleObject(game, 5, new Color().getRandomColor(), 500, 500, 200, 0, 0));
+game.add(new circleObject(game, 5, new Color().getRandomColor(), 1000, 250, 15, 0, 0));
+
+game.nBodySimulator = new nBody(1, 0.001, 100, game.gameObjects);
+
+let simulatorUpdates = [
+    game.nBodySimulator.updatePositionVectors(),
+    game.nBodySimulator.updateVelocityVectors(),
+    game.nBodySimulator.updateAccelerationVectors()
+];
+
 game.start(ctx); //start the game.
 
-start(game, ctx);
+start(game, ctx, simulatorUpdates); //start the gameLoop
 
