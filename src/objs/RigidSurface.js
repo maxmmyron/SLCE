1a4fdcc8f02a3434ed5c167676acb9debbd988a3
maxmmyron <maxmmyron@gmail.com>
2019-11-04 10:52:25 -0500

    moved old objects to new folder to help phase out use

---
+++ src/objs/RigidSurface.js
@@ -0,0 +1,36 @@
+/**
+ * Rigid Surfaces are rectangular surfaces that denote the playing floor. 
+ * They are a rigidly bound object that is denoted by (x,y) and the lower 
+ * right corner of the canvas, where y represents some hardcoded value 
+ * in the game settings. 
+ * 
+ * It is planned to allow a bit more freedom, like space away from the walls of the screen, 
+ * 
+ * @deprecated
+ */
+export default class rigidSurface {
+    constructor(game) {
+        this.game = game;
+
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+
+        this.style = {
+            color: game.environment.surfaces.color
+        };
+
+        this.pos = {
+            x: 0,
+            y: this.gameHeight - game.environment.surfaces.height
+        };
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.style.color;
+        ctx.fillRect(this.pos.x, this.pos.y, this.gameWidth, this.gameHeight);
+    }
+
+    update(delta){
+        if(!delta) return;
+    }
+} 
---
+++ src/objs/Shape.js
@@ -0,0 +1,27 @@
+import Controller from "../Handlers/Controller.js";
+
+/**
+ * Creates a new controller. This controller class is what is used to take control of an object and manupulate it around the screen.
+ */
+
+export default class Shape extends Controller {
+    constructor(game, posX, posY, velX, velY, color){
+        super(game);
+
+        this.game = game;
+
+        this.vel = {
+            x: velX,
+            y: velY
+        };
+
+        this.x = posX || game.gameWidth / 2;
+        this.y = posY || game.gameHeight / 2;
+
+        this.style = {
+            color: color || "rgba(0,0,0,1)"
+        };
+
+        this.constraints = {maxSpeed: 30};
+    }
+}
