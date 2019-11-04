1b89f8d44d2880ef68fabee203d167ccb555906a
maxmmyron <maxmmyron@gmail.com>
2019-11-04 10:51:00 -0500

    refactor, debug

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //imports
 import Game from "./src/core/game.js";
 import {start} from "./src/core/gameLoop.js";
-import circleObject from "./src/Objects/Circle.js";
-import Color from "./src/Math/Color.js";
+import Circle from "./src/Objects/Circle.js";
+import ColorManager from "./src/Math/ColorManager.js";
 import nBody from "./src/Physics/nBody.js";
 
 //define a new canvas given a canvas in DOM
@@ -12,44 +12,34 @@ let ctx = canvas.getContext('2d'); //define a new drawing context on our canvas
 
 let game = new Game(canvas); //create a new game
 
-/*for(var i = 0; i < 100; i++){
-    game.add(new circleObject(
-        game, Math.floor(Math.random() * 10) + 5,
-        new Color().getRandomColor(),
-        Math.random() * game.gameWidth,
-        Math.random() * game.gameHeight,
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10) - 5,
-        Math.floor(Math.random() * 10) - 5
-    ));
-}*/
+let colorManager = new ColorManager();
 
 let radius = 15;
 for(var i = 0; i < 5; i++){ //number of rows
     let xOffset = -(radius * 2) + (i * radius) + (radius * 2);
     let yOffset = (radius * 2 * (i * 0.89));
     for(var j = i; j >= 0; j--){ //balls per row
-        game.add(new circleObject(
+        game.add(new Circle(
             game,
             radius,
-            new Color().getRandomColor(),
+            colorManager.getRandomColor(),
             (game.gameWidth / 2) - (xOffset) + (j * radius * 2),
             (game.gameHeight / 4) - (yOffset),
             10,
-            0,0
+            0,0,
+            true
         ));
     }
 }
 
-game.add(new circleObject(
+game.add(new Circle(
     game,
     radius,
-    new Color().getRandomColor(),
-    game.gameWidth / 2,
-    game.gameHeight - 250,
+    colorManager.getRandomColor(),
+    game.gameWidth / 2, game.gameHeight - 250,
     10,
-    0,
-    -10
+    0, -10,
+    false, true
 ));
 
 game.nBodySimulator = new nBody(0.1, 0.001, 0.15, game.gameObjects);
