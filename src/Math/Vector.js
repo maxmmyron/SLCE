d286dec45e4ef8b25857c6dc43f05fdf38d46683
maxmmyron <maxmmyron@gmail.com>
2019-11-04 10:53:04 -0500

    many new functions to help with SAT and to smoooth transition of current objects to vector use

--- src/Math/Vector.js
+++ src/Math/Vector.js
@@ -1,3 +1,5 @@
+//Lots of this code is modified versions of the code from jriecken's SAT.js system (https://github.com/jriecken)
+
 /**
  * A Vector. Creates and handles math with vectors.
  */
@@ -28,7 +30,7 @@ export default class Vector{
      * Returns the current x and y components as array.
      */
     get(){
-        return [this.x, this.y];
+        return this;
     }
 
     /**
@@ -42,26 +44,121 @@ export default class Vector{
     }
 
     /**
+     * return a cloned vector
+     */
+    clone(){
+        return new Vector(this.x, this.y);
+    }
+
+    /**
+     * copies a given vector to the parent vector
+     * @param {Vector} vector - a given vector
+     */
+    copy(vector){
+        this.x = vector.x;
+        this.y = vector.y;
+        return this;
+    }
+
+    /**
      * adds a given vector onto the parent vector
      * @param {Vector} vector - a given vector
      */
-    addVector(vector){
-        return new Vector(this.x + vector.x, this.y + vector.y);
+    add(vector){
+        this.x += vector.x;
+        this.y += vector.y;
+        return this;
     }
 
     /**
      * subtracts a given vector onto the parent vector
      * @param {Vector} vector - a given vector
      */
-    subtractVector(vector){
-        return new Vector(this.x - vector.x, this.y - vector.y);
+    subtract(vector){
+        this.x -= vector.x;
+        this.y -= vector.y;
+        return this;
+    }
+
+    /**
+     * Scales the vector by an x and y factor
+     * @param {number} x - x scaling factor
+     * @param {?number=} y - y scaling factor. if unspecificed, y direction is scaled by x factor
+     */
+    scale(x,y){
+        this.x *= x;
+        this.y *= y || x;
+        return this;
+    }
+
+    rotate(angle){
+        let x = this.x;
+        let y = this.y;
+        this.x = x * Math.cos(angle) - y * Math.sin(angle);
+        this.y = x * Math.sin(angle) - y * Math.cos(angle);
+        return this;
+    }
+
+    reverse(){
+        this.x = -this.x;
+        this.y = -this.y;
+        return this;
+    }
+
+    /**
+     * normalizes the parent vector to a length of one.
+     */
+    normalize(){
+        var length = this.len();
+        if(length > 0){
+            this.x = this.x / length;
+            this.y = this.y / length;
+        }
+        return this;
+    }
+
+    /**
+     * Projects the parent vector onto another vector
+     * @param {Vector} vector - a given vector
+     */
+    project(vector){
+        let a = this.dot(vector) / this.len2();
+        this.x = a * other.x;
+        this.y = a * other.y;
+        return this;
+    }
+
+    /**
+     * Project the parent vector onto a vector of unit length. Slightly more efficent when dealing with vectors of unit length
+     * @param {Vector} vector - a given vector
+     */
+    projectN(vector){
+        let a = this.dot(vector);
+        this.x = a * other.x;
+        this.y = a * other.y;
+        return this;
+    }
+
+    reflect(axis){
+        let x = this.x, y = this.y;
+        this.project(axis).scale(2);
+        this.x -= x;
+        this.y -= y;
+        return this;
+    }
+    relectN(axis){
+        let x = this.x, y = this.y;
+        this.projectN(axis).scale(2);
+        this.x -= x;
+        this.y -= y;
+        return this;
     }
 
     /**
      * returns the dot product of a given vector and the parent vector
      * @param {Vector} vector - a given vector
      */
-    dotProductOfVector(vector){
+    dot(vector){
         return (this.x * vector.x + this.y, vector.y);
     }
 
@@ -70,7 +167,7 @@ export default class Vector{
      * @param {Vector} vec - a given vector
      * @param {boolean} inDeg - Boolean value, if true, then angle will be returned in degrees. If false, then angle will be returned in radians.
      */
-    getVecAngleFromXY(vector, inDeg){
+    vecAngle(vector, inDeg){
         return (inDeg ? Math.atan(vector.x / vector.y) * (180 * Math.PI) : Math.atan(vector.x / vector.y));
     }
 
@@ -80,6 +177,22 @@ export default class Vector{
     getVectorMagnitude(){
         return Math.sqrt(this.x ** 2 + this.y ** 2);
     }
+
+
+    /**
+     * returns length^2 of a given vector
+     */
+    len2(){
+        return this.dot(this);
+    }
+
+    /**
+     * 
+     */
+    len(){
+        return Math.sqrt(this.len2());
+    }
+
 
     /** draws a new vector on a canvas
      * @param {Context} ctx - a given canvas
