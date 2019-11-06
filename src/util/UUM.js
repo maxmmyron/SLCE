ff28f852a36ea22738b8b80c45b6cd9b9d2d9b6d
maxmmyron <maxmmyron@gmail.com>
2019-11-05 21:08:02 -0500

    created a universal utilities manager

---
+++ src/util/UUM.js
@@ -0,0 +1,16 @@
+import ColorManager from "../Math/ColorManager.js";
+import Temps from "./Temps.js";
+import Helper from "./Helper.js";
+
+/**
+ * UUM - Universal Util Manager. Manages utilities and classes that do not need to be replicated many times over. 
+ * This is more efficent that just defining one of the below objects multiple times because it keeps one instance of it. 
+ * Since only one instance of these are ever needed, it unloads unnecessary bulk. yay.
+ */
+export default class UUM{
+    constructor(){
+        this.universalColorManager = new ColorManager();
+        this.tempSystems = new Temps();
+        this.helper = new Helper();
+    }
+}
