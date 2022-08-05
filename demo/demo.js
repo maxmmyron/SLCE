a8212e264a5b57d3ff8cd8adeffe609e790ec130
maxmmyron <maxmmyron@gmail.com>
2022-08-05 12:43:10 -0400

    add demo files

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const comp = () => {
     const element = document.createElement('div')
 
@@ -7,3 +8,27 @@ const comp = () => {
 }
 
 document.body.appendChild(comp())
+=======
+import Engine, { actors } from "../src/core/Engine";
+import Actor from "../src/Objects/Actor";
+
+const engine = new Engine();
+
+const actor = new Actor(
+  (ctx) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, 5, 0, Math.PI * 2, false);
+    ctx.fill();
+  },
+  () => {
+    pos.x += 5;
+  }
+);
+
+actors.push(actor);
+
+engine.start();
+
+setTimeout(() => engine.pause(), 2500);
+>>>>>>> 1f360a7 (add demo files)
