63a40feabde70b13a8bd6ce28c0f016f6dfde3af
maxmmyron <maxmmyron@gmail.com>
2022-08-05 12:43:04 -0400

    add actor

---
+++ src/Objects/Actor.js
@@ -0,0 +1,25 @@
+import { environment } from "../core/Engine";
+
+/**
+ * An actor function represents an actor that can be placed within the canvas.
+ * @param {Function} draw a draw function that is called every frame
+ * @param {Function} update an update function that is called every frame
+ * @param {Object} options optional arguments for velocity and position
+ */
+const Actor = (draw, update, options = {}) => {
+    // utilize nullish coalescing operator to set default values
+    // by checking if vel/pos values are null or undefined
+    let vel = options.vel ?? {x:0, y:0};
+    let pos = options.pos ?? {x:0, y:0};
+
+    const draw = (ctx) => {
+        draw(ctx);
+    }
+    
+    const update = () => {
+        update();
+        vel += environment.physics.accel;
+    }      
+}
+
+export default Actor
