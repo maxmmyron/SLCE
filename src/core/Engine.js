c02c1037416893600508183fdbb30e115cbb7705
maxmmyron <maxmmyron@gmail.com>
2022-08-14 19:31:19 -0400

    improved pause state & documentation

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -13,6 +13,7 @@ export default class Engine {
 
     /**
      * Canvas context for canvas draw calls
+     * @type {CanvasRenderingContext2D}
      */
     this.ctx = canvasDOM.getContext("2d");
 
@@ -33,7 +34,7 @@ export default class Engine {
   }
 
   // ****************************************************************
-  // Private defs
+  // Public defs
 
   /**
    * An array of actors to be updated and drawn by the canvas
@@ -45,14 +46,23 @@ export default class Engine {
    */
   start = () => {
     this.#fixDPI();
-    requestAnimationFrame(this.#update);
+    this.#isPaused = false;
+    this.#animationFrameID = requestAnimationFrame(this.#update);
   }
 
   /**
    * Pauses engine
    */
   pause = () => {
-    cancelAnimationFrame(this.#update);
+    cancelAnimationFrame(this.#animationFrameID);
+    this.#isPaused = true;
+  }
+
+  /**
+   * Gets state of isPaused property
+   */
+  get isPaused() {
+    return this.#isPaused;
   }
 
   // ****************************************************************
@@ -69,7 +79,7 @@ export default class Engine {
    */
   #debug = {
     FPS: 0,
-    showFPS: false,
+    showFPS: true,
   };
 
   /**
@@ -79,6 +89,22 @@ export default class Engine {
    * @type {number}
    */
   #prevTimestamp = 0;
+
+  /**
+   * holds ID of animation frame request
+   * 
+   * @private
+   * @type {Number}
+   */
+  #animationFrameID = 0;
+
+  /**
+   * Stores whether Engine is paused or not
+   * 
+   * @private
+   * @type {Boolean}
+   */
+  #isPaused = false;
 
   /**
    * Initializes canvas and sets up event listeners
@@ -98,31 +124,35 @@ export default class Engine {
 
   /**
    * keeps track of FPS and updates all relevant actors
+   * 
    * @private
-   * @param {Number} timestamp - timestamp of current frame
+   * @param {DOMHighResTimeStamp} timestamp - timestamp of current frame
    */
   #update = (timestamp) => {
+    if (this.#isPaused) return;
+
     // calculate time between frames
-    let dT = timestamp - this.#prevTimestamp;
+    let dt = timestamp - this.#prevTimestamp;
     this.#prevTimestamp = timestamp;
 
     // calculate current FPS
-    this.#debug.FPS = 1000 / dT;
+    this.#debug.FPS = 1000 / dt;
 
     // call draw method to draw relevant actors
     this.#draw(this.ctx);
 
     // update relevant actors
-    this.actors.forEach(actor => actor.update(dT));
+    this.actors.forEach(actor => actor.update(dt, this.environment));
 
     // filter actors array by those that are NOT queued for disposal
     this.actors.filter(actor => !actor.disposalQueued);
 
-    requestAnimationFrame(update);
+    this.#animationFrameID = requestAnimationFrame(this.#update);
   }
 
   /**
    * draws all relevant actors onto canvas
+   * 
    * @private
    * @param {CanvasRenderingContext2D} ctx - rendering context of canvas
    */
@@ -137,33 +167,37 @@ export default class Engine {
 
     // Draw FPS on screen, if enabled
     if (this.#debug.showFPS) {
-      this.ctx.fillStyle = "#000000"; // TODO: remove magic number (i.e. dynamically set color to opposite of background color)
-      if (!isNaN(this.#debug.FPS))
+      this.ctx.fillStyle = "#333333"; // TODO: remove magic number (i.e. dynamically set color to opposite of background color)
+      if (!isNaN(this.#debug.FPS)) {
         this.ctx.fillText("FPS: " + Math.round(this.#debug.FPS), 5, 15);
+        this.ctx.fillText("dt: " + (1000 / this.#debug.FPS), 5, 25);
+      }
     }
+
   }
 
   /**
    * fixes DPI of canvas
+   * 
    * @private
    * @param {HTMLCanvasElement} canvasDOM - DOM element to recalculate DPI for
    * @returns {[number, number]} - [scaledWidth, scaledHeight]
    */
   #fixDPI = () => {
     let dpi = window.devicePixelRatio;
-  
+
     // set current computed canvas dimensions
     var style_width = getComputedStyle(this.canvasDOM).getPropertyValue("width").slice(0, -2); //get width attribute
     var style_height = getComputedStyle(this.canvasDOM).getPropertyValue("height").slice(0, -2); //get height attribute
-  
+
     // scale dimensions by DPI
     var w = style_width * dpi;
     var h = style_height * dpi;
-  
+
     // set canvas element dimensions to scaled dimensions
     this.canvasDOM.setAttribute('width', w);
     this.canvasDOM.setAttribute('height', h);
-  
+
     return [w, h];
   }
 }
