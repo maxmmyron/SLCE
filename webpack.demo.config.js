c6460080819cfe2c5b3a09eea49eab542aacf933
maxmmyron <maxmmyron@gmail.com>
2022-08-14 14:12:33 -0400

    fix spacing

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -1,15 +1,15 @@
 const path = require("path")
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: "./demo/demo.js"
-    },
-    devServer: {
-        "static": "./demo",
-    },
-    output: {
-        filename: "bundle.demo.js",
-        path: path.resolve(__dirname, "demo"),
-    }
+  mode: "development",
+  entry: {
+    index: "./demo/demo.js"
+  },
+  devServer: {
+    "static": "./demo",
+  },
+  output: {
+    filename: "bundle.demo.js",
+    path: path.resolve(__dirname, "demo"),
+  }
 }
