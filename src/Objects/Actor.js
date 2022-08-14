8b93cc5fee6148565c00d28dbea26a270f4dfa81
maxmmyron <maxmmyron@gmail.com>
2022-08-14 14:21:57 -0400

    rename args

--- src/Objects/Actor.js
+++ src/Objects/Actor.js
@@ -6,20 +6,20 @@ import { environment } from "../core/Engine";
  * @param {Function} update an update function that is called every frame
  * @param {Object} options optional arguments for velocity and position
  */
-const Actor = (drawFn, updateFn, options = {}) => {
-    // utilize nullish coalescing operator to set default values
-    // by checking if vel/pos values are null or undefined
-    let vel = options.vel ?? {x:0, y:0};
-    let pos = options.pos ?? {x:0, y:0};
+const Actor = (drawCallback, updateCallback, options = {}) => {
+  // utilize nullish coalescing operator to set default values
+  // by checking if vel/pos values are null or undefined
+  let vel = options.vel ?? { x: 0, y: 0 };
+  let pos = options.pos ?? { x: 0, y: 0 };
 
-    const draw = (ctx) => {
-        drawFn(ctx);
-    }
-    
-    const update = () => {
-        updateFn();
-        vel += environment.physics.accel;
-    }      
+  const draw = (ctx) => {
+    drawCallback(ctx);
+  }
+
+  const update = () => {
+    updateCallback();
+    vel += environment.physics.accel;
+  }
 }
 
 export default Actor
