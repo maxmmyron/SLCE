5d8e9efaca065f1b3929bc00108ede1bd536208b
maxmmyron <maxmmyron@gmail.com>
2022-08-14 19:31:39 -0400

    updated demo (to playable state)

--- demo/demo.js
+++ demo/demo.js
@@ -4,20 +4,45 @@ import Actor from "../src/Objects/Actor.js";
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
 
-const actor = Actor(
+engine.environment.physics.accel.y = 3;
+
+const actorA = new Actor(
   (ctx) => {
     ctx.fillStyle = "#000000";
     ctx.beginPath();
-    ctx.arc(pos.x, pos.y, 5, 0, Math.PI * 2, false);
+    ctx.arc(actorA.pos.x, actorA.pos.y, 5, 0, Math.PI * 2, false);
     ctx.fill();
   },
   (dt) => {
-    pos.x += 5 * dt;
+    actorA.pos.x += 5 / dt;
+  },
+  {
+    pos: { x: 25, y: 50 },
   }
 );
 
-engine.actors.push(actor);
+const actorB = new Actor(
+  (ctx) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(actorB.pos.x, actorB.pos.y, 5, 0, Math.PI * 2, false);
+    ctx.fill();
+  },
+  (dt) => {
+    actorB.pos.y += actorB.vel.y / dt;
+    if(actorB.pos.y > engine.environment.height) {
+      actorB.pos.y = 0;
+    }
+  },
+  {
+    pos: { x: 500, y: 50 },
+  }
+);
+
+engine.actors.push(actorA);
+engine.actors.push(actorB);
 
 engine.start();
 
 setTimeout(() => engine.pause(), 2500);
+setTimeout(() => engine.start(), 3500);
