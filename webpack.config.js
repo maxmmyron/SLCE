956bcc9e4f21fd03340816d2d1158d4e09f70c47
maxmmyron <maxmmyron@gmail.com>
2022-08-14 14:12:52 -0400

    fix spacing

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/core/Engine.js'
-    },
-    devServer: {
-        "static": "./demo",
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    }
+  mode: 'development',
+  entry: {
+    index: './src/core/Engine.js'
+  },
+  devServer: {
+    "static": "./demo",
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  }
 }
