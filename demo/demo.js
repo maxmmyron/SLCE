f140826b0c71bf36583a2c0c4ac5b868537a44bc
maxmmyron <maxmmyron@gmail.com>
2022-08-21 22:53:15 -0400

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,8 @@
 import Engine from "../src/core/Engine.js";
 import { add, sub, mult, vec } from "../src/Math/Vector.js";
 import Actor from "../src/Objects/Actor.js";
+import TextureLayer from "../src/util/TextureLayer.js";
+import test from "./testing.png";
 
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
@@ -8,20 +10,36 @@ const engine = new Engine(canvas);
 engine.environment.physics.accel.y = 3;
 
 const actorA = new Actor(
-  (ctx, interp) => {
+  (ctx) => {
     ctx.fillStyle = "#000000";
     ctx.beginPath();
     ctx.arc(actorA.pos.x, actorA.pos.y, 5, 0, Math.PI * 2, false);
     ctx.fill();
   },
-  (timestep) => {
-    actorA.pos.y += actorA.vel.y / timestep;
-    if(actorA.pos.y > engine.environment.height) {
-      actorA.pos.y = 0;
+  (dt) => {
+    actorA.pos.x += 5 / dt;
+  },
+  {
+    pos: vec(25, 50),
+    textures: [new TextureLayer(test)],
+  }
+);
+
+const actorB = new Actor(
+  (ctx) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(actorB.pos.x, actorB.pos.y, 5, 0, Math.PI * 2, false);
+    ctx.fill();
+  },
+  (dt) => {
+    actorB.pos.y += actorB.vel.y / dt;
+    if (actorB.pos.y > engine.environment.height) {
+      actorB.pos.y = 0;
     }
   },
   {
-    pos: vec(500,50),
+    pos: vec(500, 50),
   }
 );
 
@@ -30,14 +48,14 @@ engine.actors.push(actorA);
 const handlePause = () => console.log("paused");
 const handleResume = () => console.log("resumed");
 
-engine.addHandler("pause", handlePause)
-engine.addHandler("resume", handleResume)
+// engine.addHandler("pause", handlePause)
+// engine.addHandler("resume", handleResume)
 
 const handleUpdate = (dt) => {
   console.log(dt);
-}
+};
 
-engine.addHandler("update", handleUpdate);
+// engine.addHandler("update", handleUpdate);
 
 engine.start();
 
@@ -47,11 +65,11 @@ setTimeout(() => engine.resume(), 3500);
 setTimeout(() => engine.pause(), 4500);
 setTimeout(() => engine.resume(), 5500);
 
-setTimeout(() => {
-  engine.removeHandler("pause", handlePause);
-  engine.removeHandler("resume", handleResume);
-  engine.removeHandler("update", handleUpdate);
-}, 6000);
+// setTimeout(() => {
+//   engine.removeHandler("pause", handlePause);
+//   engine.removeHandler("resume", handleResume);
+//   engine.removeHandler("update", handleUpdate);
+// }, 6000);
 
 setTimeout(() => engine.pause(), 6500);
 setTimeout(() => engine.resume(), 7500);
