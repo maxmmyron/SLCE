575168c8efb89c102563588b0737ca3ed9450cd9
maxmmyron <maxmmyron@gmail.com>
2022-08-22 21:18:29 -0400

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -9,61 +9,75 @@ const engine = new Engine(canvas);
 
 engine.environment.physics.accel.y = 3;
 
-const actorA = new Actor(
-  (ctx) => {
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(actorA.pos.x, actorA.pos.y, 5, 0, Math.PI * 2, false);
-    ctx.fill();
-  },
+const actorA = new Actor({
+  pos: vec(25, 50),
+  textures: [new TextureLayer(test)],
+});
+
+engine.start();
+
+actorA.eventHandler.addHandler(
+  "preload",
+  () =>
+    new Promise((resolve, reject) => setTimeout(() => resolve("done!"), 2000))
+);
+
+actorA.eventHandler.addHandler("draw", (ctx) => {
+  ctx.fillStyle = "#000000";
+  ctx.beginPath();
+  ctx.arc(actorA.pos.x, actorA.pos.y, 5, 0, Math.PI * 2, false);
+  ctx.fill();
+});
+
+actorA.eventHandler.addHandler(
+  "update",
   (dt) => {
     actorA.pos.x += 5 / dt;
   },
   {
     pos: vec(25, 50),
-    textures: [new TextureLayer(test)],
   }
 );
 
-const actorB = new Actor(
-  (ctx) => {
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(actorB.pos.x, actorB.pos.y, 5, 0, Math.PI * 2, false);
-    ctx.fill();
-  },
-  (dt) => {
-    actorB.pos.y += actorB.vel.y / dt;
-    if (actorB.pos.y > engine.environment.height) {
-      actorB.pos.y = 0;
-    }
-  },
-  {
-    pos: vec(500, 50),
-  }
-);
+engine.addActor(actorA);
 
-engine.actors.push(actorA);
+// const actorB = new Actor(
+//   (ctx) => {
+//     ctx.fillStyle = "#000000";
+//     ctx.beginPath();
+//     ctx.arc(actorB.pos.x, actorB.pos.y, 5, 0, Math.PI * 2, false);
+//     ctx.fill();
+//   },
+//   (dt) => {
+//     actorB.pos.y += actorB.vel.y / dt;
+//     if(actorB.pos.y > engine.environment.height) {
+//       actorB.pos.y = 0;
+//     }
+//   },
+//   {
+//     pos: vec(500,50),
+//   }
+// );
 
-const handlePause = () => console.log("paused");
-const handleResume = () => console.log("resumed");
+// engine.actors.push(actorB);
+
+// const handlePause = () => console.log("paused");
+// const handleResume = () => console.log("resumed");
 
 // engine.addHandler("pause", handlePause)
 // engine.addHandler("resume", handleResume)
 
-const handleUpdate = (dt) => {
-  console.log(dt);
-};
+// const handleUpdate = (dt) => {
+//   console.log(dt);
+// }
 
 // engine.addHandler("update", handleUpdate);
 
-engine.start();
+// setTimeout(() => engine.pause(), 2500);
+// setTimeout(() => engine.resume(), 3500);
 
-setTimeout(() => engine.pause(), 2500);
-setTimeout(() => engine.resume(), 3500);
-
-setTimeout(() => engine.pause(), 4500);
-setTimeout(() => engine.resume(), 5500);
+// setTimeout(() => engine.pause(), 4500);
+// setTimeout(() => engine.resume(), 5500);
 
 // setTimeout(() => {
 //   engine.removeHandler("pause", handlePause);
@@ -71,5 +85,5 @@ setTimeout(() => engine.resume(), 5500);
 //   engine.removeHandler("update", handleUpdate);
 // }, 6000);
 
-setTimeout(() => engine.pause(), 6500);
-setTimeout(() => engine.resume(), 7500);
+// setTimeout(() => engine.pause(), 6500);
+// setTimeout(() => engine.resume(), 7500);
