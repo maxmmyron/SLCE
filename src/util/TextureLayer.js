f8070094390717d3010de177f3b7b4b903270144
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-08-31 19:52:37 -0400

    cleanup

--- src/util/TextureLayer.js
+++ src/util/TextureLayer.js
@@ -6,22 +6,23 @@ import { vec } from "../Math/Vector";
 export default class TextureLayer {
   /**
    * Constructs a new TextureLayer object.
-   * 
+   *
    * @param {String} texture - path to texture image file
-   * @param {Object} options - opitonal arguments for texture layer
+   * @param {Object} initialProperties - opitonal arguments for texture layer
+   * @property {Number} initialProperties.x - starting x position of texture layer (with respect to x pos of actor)
+   * @property {Number} initialProperties.y - starting y position of texture layer (with respect to y pos of actor)
+   * @property {Number} initialProperties.width - width of texture layer
+   * @property {Number} initialProperties.height - height of texture layer
+   * @property {Number} initialProperties.zIndex - z-index of texture layer. -1 will draw texture behind actor.
    */
-  constructor(path, options = {}) {
-
+  constructor(path, initialProperties = {}) {
     this.#path = path || null;
 
     // apply any options passed in; keep defaults if not provided
-    this.options = {
-      pos: options.pos ?? vec(),
-      size: options.size ?? vec(48, 48),
-      vel: options.vel ?? vec(),
-      rotation: options.rotation ?? 0,
-      opacity: options.opacity ?? 1,
-      zIndex: options.zIndex ?? 0,
+    this.initialProperties = {
+      pos: initialProperties.pos ?? vec(),
+      vel: initialProperties.vel ?? vec(),
+      zIndex: initialProperties.zIndex ?? 0,
     };
   }
 
@@ -29,58 +30,45 @@ export default class TextureLayer {
   // Public defs
 
   /**
-   * optional arguments for drawing texture layer
-   * 
-   * @type {Object} options
-   * @property {Number} options.x - starting x position of texture layer (with respect to x pos of actor)
-   * @property {Number} options.y - starting y position of texture layer (with respect to y pos of actor)
-   * @property {Number} options.width - width of texture layer
-   * @property {Number} options.height - height of texture layer
-   * @property {Number} options.rotation - rotation of texture layer
-   * @property {Number} options.opacity - opacity of texture layer
-   * @property {Number} options.zIndex - z-index of texture layer. -1 will draw texture behind actor.
-   */
-  options;
-
-  /**
    * ImageBitmap of texture layer
-   * 
+   *
    * @type {ImageBitmap}
    */
   imageBitmap;
+
+  /**
+   * resolves an image bitmap from an image file path
+   */
+  resolveImageBitmap = () => {
+    return new Promise((resolve, reject) => {
+      if (!this.#path) return reject(new Error("No path provided"));
+
+      const image = new Image();
+      image.src = this.#path;
+      image.crossOrigin = "anonymous";
+
+      image.onload = () => {
+        // create image bitmap from image file
+        createImageBitmap(image)
+          .then((imageBitmap) => {
+            this.imageBitmap = imageBitmap;
+            resolve(imageBitmap);
+          })
+          .catch((err) => reject(err));
+      };
+
+      image.onerror = (err) => reject(err);
+    });
+  };
 
   // ****************************************************************
   // Private defs
 
   /**
    * Path to texture image file
-   * 
+   *
    * @private
    * @type {String}
    */
   #path;
-
-  /**
-   * resolves an image bitmap from an image file path
-   */
-  resolveImageBitmap = () => new Promise((resolve, reject) => {
-    if (!this.#path)
-      return reject(new Error("No path provided"));
-
-    const image = new Image();
-    image.src = this.#path;
-    image.crossOrigin = "anonymous";
-
-    image.onload = () => {
-      // create image bitmap from image file
-      createImageBitmap(image)
-        .then(imageBitmap => {
-          this.imageBitmap = imageBitmap; 
-          resolve(imageBitmap);
-        })
-        .catch(err => reject(err));
-    };
-
-    image.onerror = (err) => reject(err);
-  });
 }
