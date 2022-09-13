5671c6ea453357f3de01d541930698153eb6217c
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-09-13 13:43:22 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -9,30 +9,49 @@ const engine = new Engine(canvas);
 
 engine.environment.physics.accel.y = 0;
 
-const actor = new Actor({
-  pos: vec(
-    engine.environment.width / 2 - 32,
-    engine.environment.height / 2 - 32
-  ),
-  vel: vec(),
-  size: vec(64, 64),
-  isDebugEnabled: true,
+const numActors = 64;
+
+[...Array(numActors)].forEach((_, i) => {
+  const sqrtNumActors = Math.sqrt(numActors);
+
+  const actor = new Actor({
+    pos: vec(
+      (engine.environment.width / sqrtNumActors) * (i % sqrtNumActors),
+      (engine.environment.height / sqrtNumActors) *
+        Math.floor(i / sqrtNumActors)
+    ),
+    vel: vec(),
+    size: vec(64, 64),
+    isDebugEnabled: true,
+    isClippedToSize: i % 2 === 0,
+  });
+
+  actor.preload(async () => {
+    actor.addTextureLayer(
+      new TextureLayer(test, {
+        isActive: true,
+        size: vec(69, 69),
+      })
+    );
+  });
+
+  engine.addActor(actor);
+
+  actor.addEventHandler("on_update", async (timestep, env) => {
+    let textures = await actor.getTextures();
+
+    if (textures.length > 0) {
+      textures[0].pos.x = Math.cos(engine.getCurrentEngineTime() / 500) * 20;
+      textures[0].pos.y = Math.sin(engine.getCurrentEngineTime() / 500) * 20;
+      textures[0].size.x =
+        Math.sin(engine.getCurrentEngineTime() / 500) * 20 + 69;
+      textures[0].size.y =
+        Math.cos(engine.getCurrentEngineTime() / 500) * 20 + 69;
+    }
+  });
 });
 
-actor.preload(
-  () => {
-    actor.addTextureLayer(
-      new TextureLayer(test, { isActive: true, size: vec(69, 69) })
-    );
-  },
-  () => {
-    engine.addActor(actor);
-  }
-);
-
 engine.start();
-
-console.log(engine.getActors());
 
 setTimeout(() => {
   engine.pause();
