3017323fb9eec3c8dc615495af75a0564a1885f3
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-05 01:53:19 +0100

    improve documentation

--- src/core/EventSubscriber.js
+++ src/core/EventSubscriber.js
@@ -1,18 +1,16 @@
 import { EVENT_IDENTIFIERS } from "./EventDispatcher";
 
 /**
- * Subscribed to and handles events dispatched by the EventDispatcher.
+ * Subscribes to valid events dispatched by the EventDispatcher, and runs specified callbacks.
+ *
+ * @class
  */
 export default class EventSubscriber {
-  /**
-   *
-   */
-  constructor() {}
-
   /**
    * An array containing a series of structs that list out subscribed events and their respective callbacks.
    *
    * @type {Array}
+   * @default []
    *
    * @example
    * [{
@@ -27,9 +25,13 @@ export default class EventSubscriber {
   subscribedEvents = [];
 
   /**
+   * Subscribed to a new event type with a callback. If the event type is already subscribed to,
+   * the callback is added to the list of callbacks for that event type.
    *
-   * @param {*} eventType
-   * @param {*} callback
+   * @param {String} eventType the event type to subscribe to
+   * @param {Function} callback a callback to execute when the event is dispatched
+   *
+   * @throws {Error} if the event type is not a valid event type
    */
   subscribe(eventType, callback) {
     // ensure event is valid from engine event list
@@ -43,20 +45,24 @@ export default class EventSubscriber {
       (subscribedEvent) => subscribedEvent.event === eventType
     );
 
-    // add a new struct to subscribedEvent array if it doesn't already exist
     if (!specifiedEvent) {
+      // add a new struct to subscribedEvent array if it doesn't already exist
       this.subscribedEvents.push({ type: eventType, callbacks: [callback] });
-    }
-
-    // add callback to subscribedEvent struct as element in array if it exists
-    else {
+    } else {
+      // add callback to subscribedEvent struct as element in array if it exists
       specifiedEvent.callbacks.push(callback);
     }
   }
 
   /**
+   * Removes a callback from a subscribed event type. If the event type has no more callbacks, the event type is removed from the subscribedEvents array.
+   * If the event type is not subscribed to, nothing happens.
+   * If a callback parameter is not specified, all callbacks for the event type are removed.
    *
-   * @param {*} eventType
+   * @param {String} eventType - event type to unsubscribe from
+   * @param {Function} callback - (optional) callback to remove from event type
+   *
+   * @throws {Error} if the event type is not a valid event type
    */
-  unsubscribe = (eventType) => {};
+  unsubscribe = (eventType, callback = null) => {};
 }
