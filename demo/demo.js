e27f57edef836463168e9698d1b6bad34539b912
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-13 18:03:11 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,6 @@
 import Engine from "../src/core/Engine.js";
 import { vec } from "../src/math/Vector.js";
 import Actor from "../src/objects/Actor.js";
-import TextureLayer from "../src/util/TextureLayer.js";
-
-import grassTexturePath from "./grassTexture.png";
-import dirtTexturePath from "./dirtTexture.png";
 
 import testingTexturePath from "./testTexture.png";
 
@@ -17,16 +13,16 @@ const ground = new Actor({
   isDebugEnabled: true,
 });
 
-ground.preload(async () => {
-  ground.addTextureLayer(
-    new TextureLayer(testingTexturePath, {
-      isActive: true,
-      size: vec(64, 64),
-      tileMode: "tile",
-      zIndex: 0,
-    })
-  );
-});
+// ground.preload(async () => {
+//   ground.addTextureLayer(
+//     new TextureLayer(testingTexturePath, {
+//       isActive: true,
+//       size: vec(64, 64),
+//       tileMode: "tile",
+//       zIndex: 0,
+//     })
+//   );
+// });
 
 engine.addActor(ground);
 
@@ -36,15 +32,15 @@ const player = new Actor({
   isDebugEnabled: true,
 });
 
-player.preload(async () => {
-  player.addTextureLayer(
-    new TextureLayer(testingTexturePath, {
-      isActive: true,
-      size: vec(48, 48),
-      zIndex: 1,
-    })
-  );
-});
+// player.preload(async () => {
+//   player.addTextureLayer(
+//     new TextureLayer(testingTexturePath, {
+//       isActive: true,
+//       size: vec(48, 48),
+//       zIndex: 1,
+//     })
+//   );
+// });
 
 player.subscribe("whilekeydown", (e) => {
   if (e.key === "ArrowRight") {
