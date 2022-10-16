f7252fd01486d6f70c12be5d2ab639f2da560514
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-16 17:55:23 +0100

    update demo to stress test

--- demo/demo.js
+++ demo/demo.js
@@ -7,52 +7,47 @@ import animationSpritemap from "./animationSpritemap.png";
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
 
-const ground = new Actor({
-  pos: vec(0, engine.environment.properties.size.y - 200),
-  size: vec(engine.environment.properties.size.x, 500),
-  isDebugEnabled: true,
-});
+const engineWidth = engine.environment.properties.size.x;
+const engineHeight = engine.environment.properties.size.y;
 
-engine.addActor(ground);
+const playerSize = vec(64, 64);
 
-const player = new Actor({
-  pos: vec(300, engine.environment.properties.size.y - 200 - 48),
-  size: vec(48, 48),
-  isDebugEnabled: true,
-});
+const playerMargin = 32;
 
-player.preload(async () => {
-  player
-    .loadTexture("animation", animationSpritemap, {
-      frameCount: 64,
-      spriteSize: vec(64, 64),
-    })
-    .then(() => {
-      player.addAnimationState("idle", "animation", {
-        frameCount: 64,
-        startIndex: 0,
-        frameDuration: 200,
-      });
-      player.setAnimationState("idle");
+const playersPerRow = engineWidth / (playerSize.x + playerMargin);
+const playersPerColumn = engineHeight / (playerSize.y + playerMargin);
+
+for (let i = 0; i < playersPerRow; i++) {
+  for (let j = 0; j < playersPerColumn; j++) {
+    const player = new Actor({
+      pos: vec(
+        i * (playerSize.x + playerMargin),
+        j * (playerSize.y + playerMargin)
+      ),
+      size: playerSize,
+      isDebugEnabled: true,
     });
-});
 
-console.log(player.textureManager.textures);
-console.log(player.animationManager.animations);
+    player
+      .preload(async () => {
+        await player.loadTexture("animation", animationSpritemap, {
+          frameCount: 64,
+          spriteSize: vec(64, 64),
+        });
+        console.log(`${i + i * j} texture loaded`);
+        player.addAnimationState("idle", "animation", {
+          frameCount: 64,
+          startIndex: 0,
+          frameDuration: 200,
+        });
+        player.setAnimationState("idle");
+      })
+      .then(() => {
+        console.log(`${i + i * j} preload finished`);
+      });
 
-player.subscribe("whilekeydown", (e) => {
-  if (e.key === "ArrowRight") {
-    player.vel.x = 5;
-  } else if (e.key === "ArrowLeft") {
-    player.vel.x = -5;
+    engine.addActor(player);
   }
-});
-
-player.update = (dt, env) => {
-  player.vel.x *= 0.85;
-  player.pos.x += player.vel.x;
-};
-
-engine.addActor(player);
+}
 
 engine.start();
