e873eae53cd6bcc3c03a95ef1aec953d17288b12
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-17 15:30:32 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,6 +4,7 @@ import Actor from "../src/objects/Actor";
 import { TextureLoader } from "../src/util/TextureLoader";
 
 import animationSpritemap from "./animationSpritemap.png";
+import characterSpritemap from "./characterSpritemap.png";
 
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
@@ -11,43 +12,68 @@ const engine = new Engine(canvas);
 const engineWidth = engine.environment.properties.size.x;
 const engineHeight = engine.environment.properties.size.y;
 
-const actorSize = vec(64, 64);
+const actorSize = vec(64, 82);
 const actorMargin = 32;
 
 const actorsPerRow = Math.floor(engineWidth / (actorSize.x + actorMargin));
 const actorsPerColumn = Math.floor(engineHeight / (actorSize.y + actorMargin));
 
-const texture = TextureLoader.getInstance().load(animationSpritemap);
+const characterTexture = TextureLoader.getInstance().load(characterSpritemap);
 
-texture.then((texture) => {
-  for (let i = 0; i < actorsPerRow; i++) {
-    for (let j = 0; j < actorsPerColumn; j++) {
-      const actor = new Actor({
-        pos: vec(
-          i * (actorSize.x + actorMargin),
-          j * (actorSize.y + actorMargin)
-        ),
-        size: actorSize,
-        isDebugEnabled: true,
-      });
+characterTexture.then((texture) => {
+  const actor = new Actor({
+    pos: vec(
+      engineWidth / 2 - (actorSize.x + actorMargin),
+      (engineHeight - actorSize.y) / 2
+    ),
+    size: actorSize,
+    isDebugEnabled: true,
+  });
 
-      actor.preload(async () => {
-        actor.addTexture("animation", texture, {
-          frameCount: 64,
-          spriteSize: vec(64, 64),
-        });
+  actor.preload(async () => {
+    actor.addTexture("walkSpritemap", texture, {
+      frameCount: 4,
+      spriteSize: vec(32, 41),
+    });
 
-        actor.addAnimationState("idle", "animation", {
-          frameCount: 64,
-          startIndex: 0,
-          frameDuration: 200,
-        });
-        actor.setAnimationState("idle");
-      });
+    actor.addAnimationState("walk", "walkSpritemap", {
+      frameCount: 4,
+      startIndex: 0,
+      frameDuration: 200,
+    });
+    actor.setAnimationState("walk");
+  });
 
-      engine.addActor(actor);
-    }
-  }
+  engine.addActor(actor);
+});
+
+const testTexture = TextureLoader.getInstance().load(animationSpritemap);
+
+testTexture.then((texture) => {
+  const actor = new Actor({
+    pos: vec(
+      engineWidth / 2 + (actorSize.x + actorMargin),
+      (engineHeight - 64) / 2
+    ),
+    size: vec(64, 64),
+    isDebugEnabled: true,
+  });
+
+  actor.preload(async () => {
+    actor.addTexture("testSpritemap", texture, {
+      frameCount: 64,
+      spriteSize: vec(64, 64),
+    });
+
+    actor.addAnimationState("testAnimation", "testSpritemap", {
+      frameCount: 64,
+      startIndex: 0,
+      frameDuration: 200,
+    });
+    actor.setAnimationState("testAnimation");
+  });
+
+  engine.addActor(actor);
 });
 
 engine.start();
