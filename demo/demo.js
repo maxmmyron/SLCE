dfb351c589432471a7a62bd70bf3381bf771d475
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-12-09 04:52:31 +0000

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,16 +11,12 @@ import characterSpritemap from "./characterSpritemap.png";
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
 
-const engineWidth = engine.environment.properties.size.x;
-const engineHeight = engine.environment.properties.size.y;
-
 const actorMargin = 32;
 
 let actorSize = vec(64, 82);
 
-let actorA = new Actor("ActorA", {
-  pos: vec(engineWidth / 2 - (actorSize.x + actorMargin), (engineHeight - actorSize.y) / 2),
-  size: actorSize,
+let actorA = new Actor(engine, "ActorA", actorSize, {
+  pos: vec(engine.canvasSize.x / 2 - (actorSize.x + actorMargin), (engine.canvasSize.y - actorSize.y) / 2),
   isDebugEnabled: true,
 });
 
@@ -28,7 +24,7 @@ actorA.preload = async () => {
   let texture = await TextureLoader.getInstance().load(characterSpritemap);
   actorA.addTexture("walkSpritemap", texture, {
     frameCount: 4,
-    spriteSize: vec(32, 41),
+    textureSize: vec(32, 41),
   });
 
   actorA.addAnimationState("walk", "walkSpritemap", {
@@ -44,9 +40,8 @@ engine.addActor(actorA);
 
 actorSize = vec(64, 64);
 
-const actorB = new Actor("ActorB", {
-  pos: vec(engineWidth / 2 + (actorSize.x + actorMargin), (engineHeight - actorSize.y) / 2),
-  size: actorSize,
+const actorB = new Actor(engine, "ActorB", actorSize, {
+  pos: vec(engine.canvasSize.x / 2 + (actorSize.x + actorMargin), (engine.canvasSize.y - actorSize.y) / 2),
   isDebugEnabled: true,
 });
 
@@ -54,7 +49,7 @@ actorB.preload = async () => {
   let texture = await TextureLoader.getInstance().load(animationSpritemap);
   actorB.addTexture("spritemap", texture, {
     frameCount: 64,
-    spriteSize: vec(64, 64),
+    textureSize: vec(64, 64),
   });
 
   actorB.addAnimationState("testAnimation", "spritemap", {
