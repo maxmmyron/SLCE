dd046295f02f533f32503bc7aaacf88b1bd029f2
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-12-20 23:26:23 -0500

    add camera & scene classes

---
+++ src/core/Camera.ts
@@ -0,0 +1,10 @@
+/**
+ * @class Camera
+ * A camera that is used to render a scene onto the engine screen.
+ */
+export class Camera {
+  // ****************************************************************
+  // ⚓ CONSTRUCTOR
+  // ****************************************************************
+  constructor() { }
+}
---
+++ src/core/Scene.ts
@@ -0,0 +1,10 @@
+/**
+ * @class Scene
+ * A collection of actors and cameras.
+ */
+export class Scene {
+  // ****************************************************************
+  // ⚓ CONSTRUCTOR
+  // ****************************************************************
+  constructor() { }
+}
