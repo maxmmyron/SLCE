05ef828e0a053a5356486fa1fd9adbc600579950
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-12-26 01:34:02 -0500

    add initial boilerplate

--- src/core/Camera.ts
+++ src/core/Camera.ts
@@ -1,10 +1,34 @@
+import { vec } from "../math/vector";
+
 /**
  * @class Camera
  * A camera that is used to render a scene onto the engine screen.
  */
 export class Camera {
   // ****************************************************************
+  // ⚓ PUBLIC DECLARATIONS
+  // ****************************************************************
+
+  readonly ID: string;
+
+  position: Vector = vec();
+
+  rotation: Vector = vec();
+
+  zoom: number = 1;
+
+  // ****************************************************************
   // ⚓ CONSTRUCTOR
   // ****************************************************************
-  constructor() { }
+  constructor(position?: Vector, rotation?: Vector, zoom?: number) {
+    this.ID = "a"; // Math.random().toString(36).substring(2, 9); or something
+
+    this.position = position || this.position;
+
+    this.rotation = rotation || this.rotation;
+
+    this.zoom = zoom || this.zoom;
+  }
+
+
 }
--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -1,10 +1,68 @@
+import Actor from "../objects/actor";
+import { Camera } from "./Camera";
+import Engine from "./engine";
+
 /**
  * @class Scene
  * A collection of actors and cameras.
  */
 export class Scene {
   // ****************************************************************
+  // ⚓ PUBLIC DECLARATIONS
+  // ****************************************************************
+
+  readonly ID: string;
+
+  // ****************************************************************
+  // ⚓ PRIVATE DECLARATIONS (w getters)
+  // ****************************************************************
+
+  private _actors: Array<Actor>;
+
+  private _camera: Camera;
+
+  // ****************************************************************
   // ⚓ CONSTRUCTOR
   // ****************************************************************
-  constructor() { }
+  constructor(engine: Engine, camera: Camera, actors: Array<Actor> = []) {
+    this.ID = Math.random().toString(36).substring(2, 9); // improve
+
+    this._camera = camera;
+
+    this._actors = actors;
+  }
+
+  // ****************************************************************
+  // ⚓ PUBLIC METHODS
+  // ****************************************************************
+
+  addActors(actors: Array<Actor>) {
+    this._actors.push(...actors);
+  }
+
+  removeActors(actors: Array<Actor>): void {
+    this._actors
+      .filter(actor => actors.includes(actor))
+      .forEach(actor => actor.isQueuedForDisposal = true);
+  }
+
+  update() { }
+
+  render() { }
+
+  // ****************************************************************
+  // ⚓ PRIVATE DECLARATION GETTERS & SETTERS
+  // ****************************************************************
+
+  get camera(): Camera {
+    return this._camera;
+  }
+
+  set camera(camera: Camera) {
+    this._camera = camera;
+  }
+
+  get actors(): Array<Actor> {
+    return this._actors;
+  }
 }
