d4ab1e72307a890214b60fe60173cca7aedd2421
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-12-26 23:59:19 -0500

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,11 @@
 // Character spritemap is an edited version of the original made by Buch: https://opengameart.org/users/buch
 
+import { Camera } from "../src/core/Camera";
 import Engine from "../src/core/engine";
+import { Scene } from "../src/core/Scene";
 import { vec } from "../src/math/vector";
 import Actor from "../src/objects/actor";
-import { TextureLoader } from "../src/util/texture_cache";
+import { TextureCache } from "../src/util/texture_cache";
 
 import animationSpritemap from "./animationSpritemap.png";
 import characterSpritemap from "./characterSpritemap.png";
@@ -11,56 +13,15 @@ import characterSpritemap from "./characterSpritemap.png";
 const canvas = document.getElementById("c");
 const engine = new Engine(canvas);
 
-const actorMargin = 32;
+const camera = new Camera("camera", engine);
 
-let actorSize = vec(64, 82);
+const scene = new Scene("Scene", engine);
 
-let actorA = new Actor(engine, "ActorA", actorSize, {
-  pos: vec(engine.canvasSize.x / 2 - (actorSize.x + actorMargin), (engine.canvasSize.y - actorSize.y) / 2),
-  isDebugEnabled: true,
-});
+console.log(Array.from(engine.scenes.values()));
 
-actorA.preload = async () => {
-  let texture = await TextureLoader.getInstance().load(characterSpritemap);
-  actorA.addTexture("walkSpritemap", texture, {
-    frameCount: 4,
-    textureSize: vec(32, 41),
-  });
+const actorA = new Actor("actorA", scene);
+const actorB = new Actor("actorB", scene);
 
-  actorA.addAnimationState("walk", "walkSpritemap", {
-    frameCount: 4,
-    startIndex: 0,
-    frameDuration: 200,
-  });
+console.log(Array.from(scene.actors.values()));
 
-  actorA.setAnimationState("walk");
-};
-
-engine.addActor(actorA);
-
-actorSize = vec(64, 64);
-
-const actorB = new Actor(engine, "ActorB", actorSize, {
-  pos: vec(engine.canvasSize.x / 2 + (actorSize.x + actorMargin), (engine.canvasSize.y - actorSize.y) / 2),
-  isDebugEnabled: true,
-});
-
-actorB.preload = async () => {
-  let texture = await TextureLoader.getInstance().load(animationSpritemap);
-  actorB.addTexture("spritemap", texture, {
-    frameCount: 64,
-    textureSize: vec(64, 64),
-  });
-
-  actorB.addAnimationState("testAnimation", "spritemap", {
-    frameCount: 64,
-    startIndex: 0,
-    frameDuration: 200,
-  });
-
-  actorB.setAnimationState("testAnimation");
-};
-
-engine.addActor(actorB);
-
-engine.start();
+// console.log(engine.getScenesByName("Scene"));
