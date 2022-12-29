f3ce18dbd04527d51b623c8934a302a44fed2140
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-12-29 13:36:10 -0500

    update demo to include texture loading

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,8 @@
 // Character spritemap is an edited version of the original made by Buch: https://opengameart.org/users/buch
 
-import { Camera } from "../src/core/Camera";
+import { Camera } from "../src/core/camera";
 import Engine from "../src/core/engine";
-import { Scene } from "../src/core/Scene";
+import { Scene } from "../src/core/scene";
 import { vec } from "../src/math/vector";
 import Actor from "../src/objects/actor";
 import { TextureCache } from "../src/util/texture_cache";
@@ -16,6 +16,8 @@ const engine = new Engine(canvas);
 const camera = new Camera("camera", engine);
 
 const scene = new Scene("Scene", engine);
+const scene2 = new Scene("Scene", engine);
+const scene3 = new Scene("Scene", engine);
 
 console.log(engine.camera)
 console.log(Array.from(engine.scenes.values()));
@@ -23,6 +25,15 @@ console.log(Array.from(engine.scenes.values()));
 const actorA = new Actor("actorA", scene);
 const actorB = new Actor("actorB", scene);
 
+TextureCache.getInstance().load(animationSpritemap).then(bitmap => {
+  console.log("Loaded bitmap: ", bitmap);
+  actorA.addTexture("texmap", bitmap, vec(64, 64), 200);
+  actorA.textureID = "texmap";
+
+  console.log(actorA.textures);
+})
+
+
 console.log(Array.from(scene.actors.values()));
 
-// console.log(engine.getScenesByName("Scene"));
+console.log("Scenes: ", engine.getScenesByName("Scene"));
