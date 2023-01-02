16f129305185eab5fc2386494a826be72bc05f70
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-01-01 21:48:17 -0500

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,8 @@
 // Character spritemap is an edited version of the original made by Buch: https://opengameart.org/users/buch
 
-import { Camera } from "../src/core/camera";
+import Camera from "../src/core/camera";
 import Engine from "../src/core/engine";
-import { Scene } from "../src/core/scene";
+import Scene from "../src/core/scene";
 import { vec } from "../src/math/vector";
 import Actor from "../src/objects/actor";
 import { TextureCache } from "../src/util/texture_cache";
@@ -35,27 +35,31 @@ actorA.preload = async () => {
   actorB.textureID = "texmap";
 };
 
+actorA.addListener("onkeydown", (e) => {
+  if (e.key === "r") {
+    actorA.vel = vec();
+    actorA.setPosition(vec(250, engine.canvasSize.y / 2 - 32));
+  }
+});
+
 actorA.addListener("whilekeydown", (e) => {
-  if (e.key === "ArrowRight") {
-    actorA.vel.x = 1;
-  }
-  if (e.key === "ArrowLeft") {
-    actorA.vel.x = -1;
-  }
-  if (e.key === "ArrowUp") {
-    actorA.vel.y = -1;
-  }
-  if (e.key === "ArrowDown") {
-    actorA.vel.y = 1;
-  }
+  if (e.key === "ArrowRight") actorA.vel.x += 0.01;
+  if (e.key === "ArrowLeft") actorA.vel.x += -0.01;
+  if (e.key === "ArrowUp") actorA.vel.y += -0.01;
+  if (e.key === "ArrowDown") actorA.vel.y += 0.01;
+
+  actorA.vel.x = Math.min(Math.max(actorA.vel.x, -0.5), 0.5);
+  actorA.vel.y = Math.min(Math.max(actorA.vel.y, -0.5), 0.5);
 });
 
 actorA.addListener("ontick", (e) => {
-  actorA.vel.x *= 0.9;
-  actorA.vel.y *= 0.9;
+  actorA.pos.x += actorA.vel.x * e.deltaTime;
+  actorA.pos.y += actorA.vel.y * e.deltaTime;
 });
 
 actorB.addListener("ontick", (e) => {
+  actorB.pos.x += actorB.vel.x * e.deltaTime;
+
   if (actorB.pos.x > engine.canvasSize.x + 64) {
     actorB.pos.x = 0;
   }
