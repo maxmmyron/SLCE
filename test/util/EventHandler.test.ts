3b84a7453fc4636ec8648c8b227ac58de3db2f95
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-15 00:17:44 +0100

    update tests (they all fail rn lol)

--- test/util/EventHandler.test.ts
+++ test/util/EventHandler.test.ts
@@ -1,25 +1,42 @@
 import { describe, it, expect, expectTypeOf, vi } from "vitest";
 import EventHandler from "@/util/EventHandler";
 
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
 describe("EventHandler", () => {
+  const canvas = document.createElement("canvas");
+
   it("is instantiable", () => {
-    const eventHandler = new EventHandler();
+    const eventHandler = new EventHandler(canvas);
 
     expect(eventHandler).toBeInstanceOf(EventHandler);
     expectTypeOf(eventHandler).toEqualTypeOf<EventHandler>();
   });
 
-  describe("event callbacks", () => {
-    it("can be registered to the callback registry", () => {
-      const eventHandler = new EventHandler();
+  describe("callback registry", () => {
+    it("can register a callback", () => {
+      const eventHandler = new EventHandler(canvas);
 
       eventHandler.registerEventCallback("onmousedown", () => {});
 
       expect(eventHandler.getEventCallbacks("onmousedown").length).toBe(1);
     });
 
-    it("can be unregistered from the callback registry", () => {
-      const eventHandler = new EventHandler();
+    it("can register multiple callbacks", () => {
+      const eventHandler = new EventHandler(canvas);
+
+      eventHandler.registerEventCallback("onmousedown", () => {});
+      eventHandler.registerEventCallback("onmousedown", () => {});
+
+      expect(eventHandler.getEventCallbacks("onmousedown").length).toBe(2);
+    });
+
+    it("can unregister a callback", () => {
+      const eventHandler = new EventHandler(canvas);
 
       let callback: EngineEventCallback<"onmousedown"> = () => {};
 
@@ -31,58 +48,116 @@ describe("EventHandler", () => {
     });
   });
 
-  describe("event payloads", () => {
-    it("can be queued", () => {
-      const eventHandler = new EventHandler();
+  describe("event queue", () => {
+    it("can queue a simple event", () => {
+      const eventHandler = new EventHandler(canvas);
 
       eventHandler.queueEvent("onrender", {type: "onrender", interpolationFactor: 0});
 
-      expect(eventHandler.getQueuedEvents("onrender")).not.toBeNull();
-      expect(eventHandler.getQueuedEvents("onrender")?.type).toBe("onrender");
-      expect(eventHandler.getQueuedEvents("onrender")).toEqual({
+      expect(eventHandler.getQueuedEvents("onrender").length).toBe(1);
+      expect(eventHandler.getQueuedEvents("onrender")[0].type).toBe("onrender");
+      expect(eventHandler.getQueuedEvents("onrender")[0]).toEqual({
         type: "onrender",
         interpolationFactor: 0,
       });
     });
 
-    it("will queue related persistent events", () => {
-      const eventHandler = new EventHandler();
+    it("can queue an event with a persistent dependent", () => {
+      const eventHandler = new EventHandler(canvas);
 
       const mousedownPayload = { type: "onmousedown", x: 0, y: 0, button: 0 }
       eventHandler.queueEvent("onmousedown", mousedownPayload);
 
-      expect(eventHandler.getQueuedEvents("onmousedown")).not.toBeNull();
-      expect(eventHandler.getQueuedEvents("onmousedown")?.type).toBe("onmousedown");
-      expect(eventHandler.getQueuedEvents("onmousedown")).toEqual(mousedownPayload);
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(1);
+      expect(eventHandler.getQueuedEvents("onmousedown")[0].type).toBe("onmousedown");
+      expect(eventHandler.getQueuedEvents("onmousedown")[0]).toEqual(mousedownPayload);
 
-      expect(eventHandler.getQueuedEvents("whilemousedown")).not.toBeNull();
-      expect(eventHandler.getQueuedEvents("whilemousedown")?.type).toBe("whilemousedown");
-      expect(eventHandler.getQueuedEvents("whilemousedown")).toEqual({
+      expect(eventHandler.getQueuedEvents("whilemousedown").length).toBe(1);
+      expect(eventHandler.getQueuedEvents("whilemousedown")[0].type).toBe("whilemousedown");
+      expect(eventHandler.getQueuedEvents("whilemousedown")[0]).toEqual({
         ...mousedownPayload,
         type: "whilemousedown"
       });
     });
 
-    it("can be dequeued", () => {
-      const eventHandler = new EventHandler();
+    it("can queue multiple events", () => {
+      const eventHandler = new EventHandler(canvas);
+
+      eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 0 });
+      eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 1 });
+      eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 2 });
+
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(3);
+      expect(eventHandler.getQueuedEvents("whilemousedown").length).toBe(3);
+    });
+
+    // why dequeue when we can just filter out queue after cycle?
+    it("can deqeue a simple event", () => {
+      const eventHandler = new EventHandler(canvas);
+
+      eventHandler.queueEvent("ontick", { type: "ontick", deltaTime: 0 });
+
+      expect(eventHandler.getQueuedEvents("ontick").length).toBe(1);
+
+      // run this as a "cycle" passing
+      eventHandler.dequeueSimpleEvents();
+
+      expect(eventHandler.getQueuedEvents("ontick").length).toBe(0);
+    });
+
+    it("can dequeue an event with a persistent dependent without dequeueing that dependent", () => {
+      const eventHandler = new EventHandler(canvas);
 
       eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 0 });
 
-      expect(eventHandler.getQueuedEvents("onmousedown")).not.toBeNull();
-      expect(eventHandler.getQueuedEvents("whilemousedown")).not.toBeNull();
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(1);
+      expect(eventHandler.getQueuedEvents("whilemousedown").length).toBe(1);
 
-      eventHandler.dequeueEvent("onmousedown");
+      eventHandler.dequeueSimpleEvents();
 
-      expect(eventHandler.getQueuedEvents("onmousedown")).toBeNull();
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(0);
 
       // Persistent events are not removed from queue until the queue contains a comparibly-equivalent negator event
-      expect(eventHandler.getQueuedEvents("whilemousedown")).not.toBeNull();
+      expect(eventHandler.getQueuedEvents("whilemousedown").length).toBe(1);
+    });
+
+    it("will dequeue the comparably-equivalent persistent event when a negator event is queued", () => {
+      // in other words, onkeyup with key = "E" will dequeue whilekeydown with key = "E", but not whilekeydown with key = "Q"
+      const eventHandler = new EventHandler(canvas);
+
+      eventHandler.queueEvent("onkeydown", { type: "onkeydown", key: "E" });
+      eventHandler.queueEvent("onkeydown", { type: "onkeydown", key: "Q" });
+
+      expect(eventHandler.getQueuedEvents("onkeydown").length).toBe(2);
+      expect(eventHandler.getQueuedEvents("whilekeydown").length).toBe(2);
+
+      eventHandler.dequeueSimpleEvents();
+
+      expect(eventHandler.getQueuedEvents("onkeydown").length).toBe(0);
+      expect(eventHandler.getQueuedEvents("whilekeydown").length).toBe(2);
+
+      eventHandler.queueEvent("onkeyup", { type: "onkeyup", key: "E" });
+
+      expect(eventHandler.getQueuedEvents("onkeyup").length).toBe(1);
+      expect(eventHandler.getQueuedEvents("whilekeydown").length).toBe(1);
+    });
+
+    it("will not queue an event if the engine is paused", () => {
+      const eventHandler = new EventHandler(canvas);
+
+      eventHandler.setEnginePauseStateCallback(() => true);
+
+      eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 0 });
+
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(0);
+      expect(eventHandler.getQueuedEvents("whilemousedown").length).toBe(0);
     });
   });
 
-  describe("dispatching events", () => {
-    it("dispatches queued events", () => {
-      const eventHandler = new EventHandler();
+  // no need to test event filtering since we've already done this in event queue tests
+  describe("dispatcher", () => {
+    it("calls registered callback", () => {
+      const eventHandler = new EventHandler(canvas);
 
       const callbacks = {
         ontick: (payload: TickEventPayload) => {
@@ -98,39 +173,36 @@ describe("EventHandler", () => {
 
       eventHandler.dispatchQueue();
 
-      expect(eventHandler.getQueuedEvents("ontick")).toBeNull();
+      expect(eventHandler.getQueuedEvents("ontick").length).toBe(0);
       expect(ontickSpy).toHaveBeenCalled();
     });
 
-    it("does not remove independently persistent event from queue", () => {
-      const eventHandler = new EventHandler();
+    it("calls multiple registered callbacks under a single event type", () => {
+      const eventHandler = new EventHandler(canvas);
 
       const callbacks = {
-        onmousedown: (payload: MouseEventPayload) => {
-          expectTypeOf(payload).toEqualTypeOf<MouseEventPayload>();
+        onmousedownA: (payload: MouseDownEventPayload) => {
+          expectTypeOf(payload).toEqualTypeOf<MouseDownEventPayload>();
         },
-        whilemousedown: (payload: MouseEventPayload) => {
-          expectTypeOf(payload).toEqualTypeOf<MouseEventPayload>();
+        onmousedownB: (payload: MouseDownEventPayload) => {
+          expectTypeOf(payload).toEqualTypeOf<MouseDownEventPayload>();
         }
       };
 
-      const mousedownSpy = vi.spyOn(callbacks, "onmousedown");
-      const whilemousedownSpy = vi.spyOn(callbacks, "whilemousedown");
+      const onmousedownASpy = vi.spyOn(callbacks, "onmousedownA");
+      const onmousedownBSpy = vi.spyOn(callbacks, "onmousedownB");
 
-      eventHandler.registerEventCallback("onmousedown", callbacks.onmousedown);
-      eventHandler.registerEventCallback("whilemousedown", callbacks.whilemousedown);
+      eventHandler.registerEventCallback("onmousedown", callbacks.onmousedownA);
+      eventHandler.registerEventCallback("onmousedown", callbacks.onmousedownB);
 
       eventHandler.queueEvent("onmousedown", { type: "onmousedown", x: 0, y: 0, button: 0 });
 
       eventHandler.dispatchQueue();
 
-      expect(eventHandler.getQueuedEvents("onmousedown")).toBeNull();
-      expect(eventHandler.getQueuedEvents("whilemousedown")).not.toBeNull();
+      expect(eventHandler.getQueuedEvents("onmousedown").length).toBe(0);
 
-      expect(mousedownSpy).toHaveBeenCalled();
-      expect(whilemousedownSpy).toHaveBeenCalled();
+      expect(onmousedownASpy).toHaveBeenCalled();
+      expect(onmousedownBSpy).toHaveBeenCalled();
     });
-
-    it("removes persistent event from queue if comparison event is dispatched");
   });
 });
