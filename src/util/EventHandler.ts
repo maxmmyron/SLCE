bf8e04e310f9b5c1c943fe7057345af748ae9b2d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-14 22:35:39 +0100

    finish implementation (pending tests)

--- src/util/EventHandler.ts
+++ src/util/EventHandler.ts
@@ -28,6 +28,59 @@ export default class EventHandler implements EventHandlerable {
     "onrender": null
   };
 
+  private resizeObserver: ResizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    entries.forEach((entry: ResizeObserverEntry) => {
+      const { width, height } = entry.contentRect;
+
+      this.queueEvent("onresize", { type: "onresize", width, height });
+    });
+  });
+
+  private canvas: HTMLCanvasElement;
+
+  private canvasEventHandlerMap: Map<string, ((event: any) => any) | null> = new Map([
+    ["mousedown", (event: any) => {
+      event = event as MouseEvent;
+      this.queueEvent("onmousedown", { type: "onmousedown", button: event.button, x: event.offsetX, y: event.offsetY });
+    }],
+    ["mouseup", (event: any) => {
+      event = event as MouseEvent;
+      this.queueEvent("onmouseup", { type: "onmouseup", button: event.button, x: event.offsetX, y: event.offsetY });
+    }],
+    ["mousemove", (event: any) => {
+      event = event as MouseEvent;
+      this.queueEvent("onmousemove", { type: "onmousemove", button: event.button, x: event.offsetX, y: event.offsetY });
+    }],
+    ["keydown", (event: any) => {
+      event = event as KeyboardEvent
+      this.queueEvent("onkeydown", { type: "onkeydown", key: event.key });
+    }],
+    ["keyup", (event: any) => {
+      event = event as KeyboardEvent
+      this.queueEvent("onkeyup", { type: "onkeyup", key: event.key });
+    }],
+    ["resize", null]
+  ]);
+
+  private enginePauseStateCallback: () => boolean = () => false;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+
+    this.attachEventListeners
+  }
+
+  destroy(): void {
+    this.detachEventListeners();
+
+    for (const type of Object.keys(this.callbackRegistry)) {
+      this.callbackRegistry[type as keyof EngineEventHandlersEventMap] = [];
+      this.queuedEvents[type as keyof EngineEventHandlersEventMap] = null;
+    }
+
+    this.resizeObserver.disconnect();
+  }
+
   registerEventCallback = <Type extends keyof EngineEventHandlersEventMap>(type: Type, callback: (payload: EngineEventHandlersEventMap[Type]) => any): void => {
     this.callbackRegistry[type].push(callback);
   };
@@ -37,6 +90,8 @@ export default class EventHandler implements EventHandlerable {
   }
 
   queueEvent<Type extends keyof EngineEventHandlersEventMap>(type: Type, payload: EngineEventHandlersEventMap[Type]): void {
+    if(!this.enginePauseStateCallback()) return;
+
     switch (type) {
       case "onmousedown":
         this.queuedEvents["whilemousedown"] = {
@@ -80,11 +135,36 @@ export default class EventHandler implements EventHandlerable {
     }
   }
 
-  attachEventListeners(canvas: HTMLCanvasElement): void {}
+  attachEventListeners(): void {
+    this.canvasEventHandlerMap.forEach((handler, type) => {
+      switch(type) {
+        case "resize":
+          this.resizeObserver.observe(this.canvas);
+          break;
+        default:
+          this.canvas.addEventListener(type as keyof HTMLElementEventMap, handler as (event: any) => any);
+          break;
+      }
+    });
+  }
 
-  detachEventListeners(): void {}
+  detachEventListeners(): void {
+    this.canvasEventHandlerMap.forEach((handler, type) => {
+      switch(type) {
+        case "resize":
+          this.resizeObserver.unobserve(this.canvas);
+          this.resizeObserver.disconnect();
+          break;
+        default:
+          this.canvas.removeEventListener(type as keyof HTMLElementEventMap, handler as (event: any) => any);
+          break;
+      }
+    });
+  }
 
-  setEnginePauseStateCallback(callback: () => boolean): void {}
+  setEnginePauseStateCallback(callback: () => boolean): void {
+    this.enginePauseStateCallback = callback;
+  }
 
   getEventCallbacks<Type extends keyof EngineEventHandlersEventMap>(type: Type): EngineEventCallback<Type>[] {
     return this.callbackRegistry[type];
