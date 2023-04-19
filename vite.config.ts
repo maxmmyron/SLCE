22aee4d16e555ce431d32d7692f59bd75363c191
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-19 23:09:05 +0100

    remove unnecessary imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,7 @@ import * as path from "path"
 
 export default defineConfig({
   test: {
-    setupFiles: ["./vitest.setup.ts"],
     environment: "jsdom",
-    deps: {
-      inline: ["vitest-canvas-mock"]
-    },
-    threads: false,
-    environmentOptions:{
-      jsdom: {
-        resources: "usable"
-      }
-    }
   },
   resolve: {
     alias: {
