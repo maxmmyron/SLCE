874f447224465be2c795a5ef256cab55745adba9
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-25 12:02:12 +0100

    add empty test files

---
+++ test/elements/Actor.test.ts
@@ -0,0 +1,2 @@
+import { describe, it, expect } from "vitest";
+import Actor from "@/elements/Actor";
---
+++ test/elements/Scene.test.ts
@@ -0,0 +1,2 @@
+import { describe, it, expect } from "vitest";
+import Scene from "@/elements/Scene";
