daa7b28f28ac9e313e3dc8ec17ad339c79dc87e6
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-26 16:16:19 +0100

    pass options to mock engine

--- test/mocks/Engine.mock.ts
+++ test/mocks/Engine.mock.ts
@@ -1,11 +1,11 @@
 import Engine from "@/core/Engine";
 
-export const createMockEngine = (): Engineable => {
+export const createMockEngine = (options: EngineOptions = {}): Engineable => {
   global.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
   }
 
-  return new Engine(document.createElement("canvas"));
+  return new Engine(document.createElement("canvas"), options);
 }
