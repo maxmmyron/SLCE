80eabf50fe0fadf3c8bb8ea0d9388bea840d622d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-26 18:00:15 +0100

    update mock

--- test/mocks/Engine.mock.ts
+++ test/mocks/Engine.mock.ts
@@ -18,6 +18,11 @@ export const createMockEngineInstance = (options: EngineOptions = {}): {engine: 
   const canvas = document.createElement("canvas");
 
   const engine = new Engine(canvas, options);
+  engine.start = async (): Promise<void> => {
+    if(engine.isStarted) throw new Error();
+    await Promise.all(Array.from(engine.scenes.values()).map((scene) => scene.start()));
+    engine.isStarted = true;
+  };
   const camera = new Camera("testCamera", engine);
   const scene = new Scene("testScene", engine, camera);
   const actor = new Actor("testActor", scene);
