2d216e4df099e95722d21dd5d11f63b86b1910c9
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-26 17:09:52 +0100

    improve mocks

--- test/mocks/Engine.mock.ts
+++ test/mocks/Engine.mock.ts
@@ -1,11 +1,26 @@
+import Camera from "@/core/Camera";
 import Engine from "@/core/Engine";
+import Scene from "@/elements/Scene";
+import Actor from "@/elements/Actor";
 
-export const createMockEngine = (options: EngineOptions = {}): Engineable => {
+/**
+ * Creates a new instance of an engine with an attached camera, scene, and actor.
+ * @param options optional arguments to pass to the engine
+ * @returns
+ */
+export const createMockEngineInstance = (options: EngineOptions = {}): {engine: Engine, camera: Camera, scene: Scene, actor: Actor} => {
   global.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
   }
 
-  return new Engine(document.createElement("canvas"), options);
+  const canvas = document.createElement("canvas");
+
+  const engine = new Engine(canvas, options);
+  const camera = new Camera("testCamera", engine);
+  const scene = new Scene("testScene", engine, camera);
+  const actor = new Actor("testActor", scene);
+
+  return { engine, camera, scene, actor };
 }
